f0dd2a4a0848f1f8e86b550894eb020a6c64d60a
Alvaro F Blanco <alvarofblanco@gmail.com>
2017-06-26 21:11:52 +0000

    Header added

--- hids.py
+++ hids.py
@@ -2,12 +2,15 @@
 import sys
 import subprocess
 import os
+
+def print_header():
+    print("#################### H I D S ####################")
+    
 def main():
     p=subprocess.Popen(["ls","-l", "/etc/resolv.conf"], stdout=subprocess.PIPE)
     output,err = p.communicate()
     
-    listdir = os.listdir('/tmp')
-    print listdir[0]
+    print_header()
     
     
     
