6ffd6a29479dad799ad15ed3e2cdf4b41cec72d3
Alvaro F Blanco <alvarofblanco@gmail.com>
2017-06-26 21:08:50 +0000

    popen

--- hids.py
+++ hids.py
@@ -1,9 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 import sys
 import subprocess
 import os
 def main():
-    f = os.popen('date')
-    now = f.read()
-    print "Today is ", now
+    p=subprocess.Popen(["ls","-l", "/etc/resolv.conf"], stdout=subprocess.PIPE)
+    output,err = p.communicate()
+    
+    listdir = os.listdir('/tmp')
+    print listdir[0]
+    
+    
+    
+
+if __name__=="__main__":
+	main()
+
     
