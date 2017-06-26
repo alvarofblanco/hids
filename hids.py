2d66713da33bf9b5e8f6b32e8c712e78fb7aa30c
Alvaro F Blanco <alvarofblanco@gmail.com>
2017-06-26 23:11:16 +0000

    more menu updates

--- hids.py
+++ hids.py
@@ -4,15 +4,36 @@ import subprocess
 import os
 
 def print_header():
-    print("#################### H I D S ####################\n")
+    print("#################### H I D S ####################")
     print("Elaborado por Alvaro Franco Blanco")
     
+def print_menu():
+    print("Menu")
+    print("1. Verificar archivos binarios del sistema")
+    print("0. Salir")
+
+def chk_bin():
+    print ("Checking bin....")
+    
+
 def main():
-    p=subprocess.Popen(["ls","-l", "/etc/resolv.conf"], stdout=subprocess.PIPE)
-    output,err = p.communicate()
-    
+
     print_header()
+    print_menu()
+    option = input("Option: ")
+    while option > 1 and option < 0 :
+        print("Opcion invalida")
+        option = input("Option: ")
     
+    cases = {
+        
+        '1': chk_bin, #do not use ()
+    }
+    
+    function = cases [str(option)]
+    function()
+    
+        
     
     
 
