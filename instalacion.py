7be702c3d554a1c54c09af2b62da7278250dc1ea
Alvaro F Blanco <alvarofblanco@gmail.com>
2017-06-26 23:39:03 +0000

    db modificada

--- instalacion.py
+++ instalacion.py
@@ -7,5 +7,9 @@ password = getpass.getpass()
 
 try:
     mariadb_connection = mariadb.connect("localhost",'root',password)
-except mariadb.Error
+except mariadb.Error as error:
     print("Error: {}".format(error))
+    
+cursor = mariadb_connection.cursor()
+
+#Creacion de la base de datos
