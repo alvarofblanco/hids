07cb944ac10d24858b0cfe20ee04624d80b8624c
root <root@localhost.localdomain>
2017-06-26 20:31:18 -0400

    Agregados los checksum

--- instalacion.py
+++ instalacion.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import MySQLdb as mariadb
 import getpass
+import hashlib
 
 print ("Configurando base de datos")
 password = getpass.getpass()
@@ -13,3 +14,26 @@ except mariadb.Error as error:
 cursor = mariadb_connection.cursor()
 
 #Creacion de la base de datos
+cursor.execute("DROP DATABASE hids")
+cursor.execute("CREATE DATABASE hids")
+cursor.execute("USE hids")
+cursor.execute("CREATE TABLE file_checksum(file_name varchar(40), checksum varchar(1024))")
+
+#Extraccion de los checksum de los archivos binarios del sistema
+#Checksum de passwd
+checksum = hashlib.md5(open('/etc/passwd', 'rb').read()).hexdigest()
+cursor.execute ("INSERT INTO file_checksum VALUES ('passwd', %s)", (checksum,))
+#Checksum de shadow
+checksum = hashlib.md5(open('/etc/shadow', 'rb').read()).hexdigest()
+cursor.execute ("INSERT INTO file_checksum VALUES ('shadow', %s)", (checksum,))
+#checksum de group
+checksum = hashlib.md5(open("/etc/group", 'rb').read()).hexdigest()
+cursor.execute("INSERT INTO file_checksum VALUES('group', %s)", (checksum,))
+checksum de resolv.conf
+checksum = hashlib.md5(open("/etc/resolv.conf", 'rb').read()).hexdigest()
+cursor.execute("INSERT INTO file_checksum VALUES('resolv', %s)", (checksum,))
+
+
+mariadb_connection.commit()
+cursor.close()
+mariadb_connection.close()
