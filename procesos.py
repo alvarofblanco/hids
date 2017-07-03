4bfa25846440ee368c1b9848de642335bc233fe5
Alvaro F Blanco <alvarofblanco@gmail.com>
2017-07-03 22:56:45 +0000

    intenta eliminar un proceso

--- procesos.py
+++ procesos.py
@@ -4,7 +4,7 @@ import os
 import re
 import time
 
-MAX="75.0"
+MAX="35.0"
 proc = subprocess.Popen('ps aux',shell=True,executable='/bin/bash',stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 out,err = proc.communicate()
 ps = out.split('\n')
@@ -17,10 +17,10 @@ for i in range(limit):
 	if cpu > MAX:
 		print "Alarma: Proceso %s consumiendo CPU en exceso" % datos[10]
 		os.system("kill -9 "+datos[1])
-		file = open("/var/log/hids/alarmas.log",'a')
+		file = open("/var/log/hids/alarmas.log",'aw')
 		date = time.strftime("%c")
 		file.write('['+ date +'] Alarma: "Proceso '+datos[10]+' con PID '+datos[1]+' consumiendo CPU en exceso"\n')
 		file.close()
-		file = open("/var/log/hids/prevencion.log",'a')
+		file = open("/var/log/hids/prevencion.log",'aw')
 		date = time.strftime("%c")
 		file.write('['+ date +'] "Proceso '+datos[10]+' terminado"\n')
