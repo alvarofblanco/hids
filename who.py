8176349611caf79ef68f974258fa6c74a961f11e
alvarofblanco <alvarofblanco@gmail.com>
2017-07-04 06:55:11 -0400

    blocks the ip and sends the mail

--- who.py
+++ who.py
@@ -4,53 +4,85 @@ import subprocess
 import os
 import re
 import time
-#import MySQLdb as mariadb
+import getpass
+import MySQLdb as mariadb
 
 #Modulo de deteccion de usuarios conectados. Revisa los usuarios conectados por medio
-#De la instruccion who -aH. Si alguno de esos es pts y no esta en la lista de usuarios permitidos, cancela la coneccion y agrega una excepcion a la iptables
+#De la instruccion who -aH. Si alguno de esos es pts y no esta en la lista de usuarios permitidos, cancela la conexion y agrega una excepcion a la iptables
 
-#Ejecuta el commando who y convierte la salida en cadenas de caracteres 
-exe = 'who -a'
+
+#Database connection
+print 'Ingrese la contrasena de la base de datos'
+pwd = getpass.getpass()
+try:
+	mariadb_connection = mariadb.connect("127.0.0.1", 'root', pwd, 'hids')
+except mariadb.Error as error:
+	print("ErrorL {}".format(error))
+
+cursor = mariadb_connection.cursor()
+
+
+
+#Executes who command and parse the output 
+exe = 'who'
 p = subprocess.Popen(exe,shell=True,executable='/bin/bash',stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 out,err = p.communicate()
 response = out.split('\n')
 
+
+#Check each line of the output
 for i in range(len(response)-1):
-	line = re.compile('\s+').split(response[i], 7)
+	#convert each line of the output in a list
+	line = re.compile('\s+').split(response[i], 5)
 	#print line
+	#print len(line)
 	
-	#Checks if the second value of the string is pseudo terminal slave probably with ssh
-	if line[2].startswith('pts'):
-		#String parsing
-		ip = line[7].replace('(','')
+	#if the line has 5 arguments (ip included)
+	if len(line) == 5:
+	
+		ip = line[4].replace('(','')
 		ip = ip.replace(')','')
-		pid = line[6]
 		
+		#PRUEBA
+		#ip = '192.168.1.110'
+
+		query = 'select * from user_ip where ip = \'' + ip + '\''
+		#print query
+		cursor.execute(query)
+		data = cursor.fetchone()
+		if not data:
+			
+			#Usuario extrano econtrado
+			
 		
-		#Alarms
-		date = time.strftime("%c")
-		alarm = '[' + date + '] Alerta! Conexion remota desde ' + ip + '\n'
-		prevention ='[' + date + '] Se a agregado esa exepcion de la ip '+ip+' en iptables\n'
+			#Alarms
+			date = time.strftime("%c")
+			alarm = '[' + date + '] Alerta! Conexion remota desde ' + ip + '\n'
+			prevention ='[' + date + '] Se a agregado esa exepcion de la ip '+ip+' en iptables\n'
 		
-		#Mail creation
-		file = open('mail','w')
-		file.write("Este mail usted esta recibiendo por la siguiente advertencia de seguridad: \n" + alarm + "y se han tomado las siguientes medidas: \n" + prevention)	
-		file.close()
-
-		#Alarma.log file
-		file = open('/var/log/hids/alarmas.log','a')
-		file.write(alarm)
-		file.close()
-
-		#Prevencion.log file edit
-		file = open('/var/log/hids/prevencion.log','a')
-		file.write(prevention)
-		file.close()
-
-		#Actually does what it says it has done
-		cmd = 'iptables -A INPUT -s '+ip+' -p tcp -m tcp --dport 22 -j REJECT'
-		#os.system(cmd)
-		#os.system('service iptables save')
-		#os.system('service iptables restart')
-
-		
+			#Mail creation
+			file = open('mail','w')
+			file.write("Este mail usted esta recibiendo por la siguiente advertencia de seguridad: \n" + alarm + "y se han tomado las siguientes medidas: \n" + prevention)	
+			file.close()
+	
+			#Alarma.log file
+			file = open('/var/log/hids/alarmas.log','a')
+			file.write(alarm)
+			file.close()
+	
+			#Prevencion.log file edit
+			file = open('/var/log/hids/prevencion.log','a')
+			file.write(prevention)
+			file.close()
+	
+			#Actually does what it says it has done
+			print alarm
+			print prevention
+			cmd = 'iptables -A INPUT -s '+ip+' -p tcp -m tcp --dport 22 -j REJECT'
+			os.system(cmd)
+			print 'Agregando excepcion a iptables'		
+			os.system('service iptables save')
+			print 'Reiniciando el servicio iptables'
+			os.system('service iptables restart')
+			os.system('python mail.py')
+	
