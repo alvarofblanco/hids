2f406bee3c292ed1757df52a3606830c1731669a
root <root@localhost.localdomain>
2017-07-04 02:56:16 -0400

    archivo who.py creado

--- who.py
+++ who.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
-import MySQLdb as mariadb
 import os
+import re
+import MySQLdb as mariadb
 
-
+#Modulo de deteccion de usuarios conectados. Revisa los usuarios conectados por medio
+#De la instruccion who -aH. Si alguno de esos es pts y no esta en la lista de usuarios permitidos, cancela la coneccion y agrega una excepcion a la iptables
