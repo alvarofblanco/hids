441d9febe51fdf72b3043cb63f5d26efa4f84259
Alvaro F Blanco <alvarofblanco@gmail.com>
2017-07-17 22:14:38 +0000

    mail.py documentation updated

--- mail.py
+++ mail.py
@@ -4,17 +4,21 @@ import smtplib
 import os
 from email.mime.text import MIMEText
 
-#fp = open("/tmp/mail", 'w')
-#fp.write('Hola los perros, soy el HIDS')
-#fp.close()
+#This file has the body of the mail
 fp = open("mail", 'rb')
 msg = MIMEText(fp.read())
 fp.close()
+
+#Headers of the mail
 msg['Subject'] = 'Aviso de seguridad'
 msg['From'] = 'hidsparaguay@gmail.com'
 msg['To'] = 'alvarofblanco@gmail.com'
+
+#Initialize the mail service
 s = smtplib.SMTP('smtp.gmail.com:587')
 s.starttls()
+
+#Mail and pass of the account
 s.login('hidsparaguay@gmail.com','Tiburoncin123')
 s.sendmail('hidsparaguay@gmail.com','alvarofblanco@gmail.com',msg.as_string() )
 os.system('rm -rf /tmp/mail')
