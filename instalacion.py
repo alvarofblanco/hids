7fc30a953d516bd51e4bd2ebd21ed4bcf0c198b8
Alvaro F Blanco <alvarofblanco@gmail.com>
2017-07-20 18:16:09 +0000

    sniffers file config

--- instalacion.py
+++ instalacion.py
@@ -2,6 +2,35 @@
 import MySQLdb as mariadb
 import getpass
 import hashlib
+import os
+
+#Create config files
+
+#Create configuration folder
+if not os.path.exists("/etc/hids"):
+    os.makedirs("/var/hids")
+
+#Crete the log files
+os.system("touch /var/log/hids/alarmas.log")
+os.system("touch /var/log/hids/prevencion.log")
+
+#Crete the sniffers files
+f = open('/etc/hids/sniffers.hids')
+f.write("tshark,john,kismet,tcpdump,dsniff,ettercap,ngrep,topng,snort")
+f.close()
+
+#Crete the users files
+f = open('/etc/hids/users.hids')
+nombre = raw_input("Inserte el nombre")
+
+while nombre != 0:
+    ip = raw_input("Inserte la direccion IP")
+    line = nombre + ',' +ip
+    f.write(line)
+    nombre = raw_input("Inserte el nombre")
+    
+
+
 
 print ("Configurando base de datos")
 password = getpass.getpass()
@@ -19,6 +48,7 @@ cursor.execute("CREATE DATABASE hids")
 cursor.execute("USE hids")
 cursor.execute("CREATE TABLE file_checksum(file_name varchar(40), checksum varchar(1024))")
 cursor.execute("CREATE TABLE user_ip(username varchar(40), ip varchar(15))")
+cursor.execute("CREATE TABLE sniffers(name varchar(40))")
 
 #Extraccion de los checksum de los archivos binarios del sistema
 
@@ -40,6 +70,9 @@ cursor.execute("INSERT INTO file_checksum VALUES('resolv', %s)", (checksum,))
 
 #entering the users
 cursor.execute("LOAD DATA LOCAL INFILE '/etc/hids/users.hids' INTO TABLE user_ip FIELDS TERMINATED BY ','")
+
+#entering the sniffers
+cursor.execute("LOAD DATA LOCAL INFILE '/etc/hids/sniffers.hids' INTO TABLE user_ip FIELDS TERMINATED BY ','")
 
 
 mariadb_connection.commit()
